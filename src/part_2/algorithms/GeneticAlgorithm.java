5f912177a2f0bb7140f09a8d5f4588f885477afb
aymmenkhs <aymenkhouas@gmail.com>
2021-06-05 00:21:39 +0100

    base of the genetic algorithm

--- src/part_2/algorithms/GeneticAlgorithm.java
+++ src/part_2/algorithms/GeneticAlgorithm.java
@@ -1,6 +1,7 @@
 package part_2.algorithms;
 
 import utils.Algorithm;
+import utils.sat_structure.SAT;
 
 import java.util.ArrayList;
 
@@ -11,44 +12,107 @@ public class GeneticAlgorithm implements Algorithm {
     private ArrayList<Integer> solution = null;
     private boolean goal_reached = false;
 
+    //sat instance
+    private final SAT sat;
+
     // genetic algorithm parameters
     // if "numberGenerations" = -1 then their wil be no limit for the number of iteration
     private final int numberGenerations, populationSize;
 
-    private ArrayList<Individual> population;
+    private ArrayList<Individual> population, childrens;
+    private int currentGeneration = 0;
+    private Individual bestIndividual;
 
-    public GeneticAlgorithm(int populationSize) {
+    public GeneticAlgorithm(SAT sat, int populationSize) {
+        this.sat = sat;
         this.populationSize = populationSize;
         // the default value of the number of generation is -1, in this case their wil be no limit for the number of iteration
         this.numberGenerations = -1;
-        this.population = new ArrayList<>(  );
     }
 
-    public GeneticAlgorithm(int populationSize, int numberGenerations) {
+    public GeneticAlgorithm(SAT sat, int populationSize, int numberGenerations) {
+        this.sat = sat;
         this.populationSize = populationSize;
         // if the the number of generation is specified then wer're gonna do at most "numberGenerations" iterations
         this.numberGenerations = numberGenerations;
-        this.population = new ArrayList<>(  );
+    }
+
+    private void geneticAlgorithm(){
+        // first we initialize the population then we evaluate it, sort it and take the best individual
+        this.currentGeneration = 0;
+        initializePopulation();
+        evaluatePopulation();
+        sortingPopulation();
+        this.bestIndividual = population.get(0);
+
+        while (!stopCondition()){
+            this.childrens = new ArrayList<>(  );
+
+            ArrayList<Individual> selectedParents = selection();
+            // we make a crossover between each of the selected parents
+            // we make mutation between to the children generation
+            // we evaluate the childrens then sort them
+            population = childrens;
+            evaluatePopulation();
+            sortingPopulation();
+
+            // if the best individual of this generation is better then the current best one then we take it as our best individual
+        }
     }
 
     private Individual crossOver(){
+        // we're gonna use a single point crossover to merge two individuals
         return new Individual(new ArrayList<>(  ));
     }
 
-    private void mutation(){
+    private ArrayList<Individual> selection(){
+        int toSelection = 10;
 
+        ArrayList<Individual> selected = new ArrayList<>(  );
+        int i = 0;
+        while (i<toSelection){
+            selected.add(this.population.get(i));
+            i++;
+        }
+        return selected;
     }
 
-    private void selection(){
-
+    private void evaluatePopulation(){
+        for (Individual individual: population){
+            individual.fitnessFunction(sat);
+        }
     }
 
     private void sortingPopulation(){
 
     }
 
+    private void initializePopulation(){
+        for(int i=0; i<this.populationSize; i++){
+            population.add(Individual.generateIndividual(sat.getNbVariables()));
+        }
+    }
+
+    private boolean stopCondition(){
+        if (goal_reached){
+            return true;
+        } else if (this.numberGenerations == -1){
+            return false;
+        } else {
+            return this.numberGenerations > this.currentGeneration;
+        }
+    }
+
     @Override
     public void solve() {
+        long startTime = System.nanoTime();
+
+        geneticAlgorithm();
+
+        long endTime = System.nanoTime();
+
+        long results =  (endTime - startTime);
+        this.time = (double) results/1000000000;
 
     }
 
--- src/part_2/algorithms/Individual.java
+++ src/part_2/algorithms/Individual.java
@@ -1,18 +1,40 @@
 package part_2.algorithms;
 
+import utils.sat_structure.SAT;
+
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Individual {
 
     private ArrayList<Integer> genome;
 
-    private int score;
+    private double score;
 
     public Individual(ArrayList<Integer> genome) {
         this.genome = genome;
     }
 
-    public double fitnessFunction(){
+    public double fitnessFunction(SAT sat){
+        this.score = sat.get_nb_verified_clauses(genome);
         return this.score;
+    }
+
+    private void mutation(){
+        // we're gonna change some value in the genome with a specified probability
+    }
+
+    public ArrayList<Integer> getSolution() {
+        return genome;
+    }
+
+    public static Individual generateIndividual(int nbVars){
+        Random rand = new Random();
+        ArrayList<Integer> genome = new ArrayList<>(  );
+
+        for (int i=0; i<nbVars; i++){
+            genome.add(rand.nextInt(2));
+        }
+        return new Individual(genome);
     }
 }
